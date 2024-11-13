0913e0864f0bc91563feebde7dd0693efce38503
Jorge Morales Rodríguez <jmrockero8@gmail.com>
2024-11-12 21:10:08 -0600

    Add files via upload

---
+++ live-coding-4/src/practicaLiveCoding4/Main.java
@@ -0,0 +1,29 @@
+package practicaLiveCoding4;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		FizzBuzz fizzBuzz = new FizzBuzz();
+		fizzBuzz.imprimir();
+
+	}
+
+}
+
+
+//En este reto reforzaras tus conocimientos en Java, manejando ciclos, condicionales e impresión por consola.
+//
+//1.- Construye un programa que imprima números del 1 al 100.
+//
+//2.- Cuando el número sea múltiplo de 3 imprimir Fizz en lugar del número.
+//
+//3.- Cuando el número sea un múltiplo de 5 imprimir Buzz en lugar del número.
+//
+//4.- Cuando el número sea múltiplo de 3 y de 5 imprimir FizzBuzz en lugar del número.
+//
+//5.- El programa no recibira inputs y cada número debe ir impreso en una línea.
+//
+//6.- Debes escribir el código en una clase llamada FizzBuzz que contenga por lo menos un método, imprimir.
+//
+//7.- En una clase Main, debes crear un objeto de dicha clase para imprimir el resultado.
